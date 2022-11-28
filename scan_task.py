2c4b311f36a70305d668dbf410296d69e271d254
stupidfish <2928109164@qq.com>
2022-11-28 11:04:52 +0800

    asdfd

--- scan_task.py
+++ scan_task.py
@@ -43,54 +43,59 @@ class scan_task_class:
             sendmail(self.mail, out.decode())
         except Exception:
             sendmail(self.mail, 'nuclei 扫描出现问题')
+            traceback.print_exc()
 
     def awvs_scan(self, urllist):  # awvs扫描函数
-        id_list = []
-        for it in urllist:
-            it = fixpackage(it)
-            if it is None:
-                break
-            for i in range(1, 3):
-                try:
-                    data = {
-                        "address": it,
-                        "description": it,
-                        "criticality": "10"
-                    }
-                    data_json = json.dumps(data)
-                    targets_api = '/api/v1/targets'
-                    r = requests.post(self.url + targets_api, data=data_json, headers=self.headers,
-                                      verify=False).text  # 发送扫描任务
-                    id_list.append(json.loads(r)['target_id'])
-                    # targets_adv_conf_api = '/api/v1/targets/%s/configuration' % json.loads(r)['target_id']  #
-                    # 高级配置api data_proxy_api_conf_set = {"proxy": {"enabled": True, "address": "43.228.71.245",
-                    # "protocol": "http", "port": 1111 } } r_conf_proxy = requests.patch(url + targets_adv_conf_api,
-                    # data=json.dumps(data_proxy_api_conf_set), verify=False, headers=headers).text
-
-                    api_run = '/api/v1/scans'
-                    data_run = {
-                        "target_id": json.loads(r)['target_id'],
-                        "profile_id": "11111111-1111-1111-1111-111111111112",
-                        "schedule":
-                            {"disable": False,
-                             "start_date": None,
-                             "time_sensitive": False
-                             }
-                    }
-                    # print(json.dumps(data_run))
-                    r_run = requests.post(self.url + api_run, data=json.dumps(data_run),
-                                          verify=False,
-                                          headers=self.headers).text
-                    # print(r_run)
+        try:
+            id_list = []
+            for it in urllist:
+                it = fixpackage(it)
+                if it is None:
                     break
-                except Exception:
-                    print_err(it)
-                    traceback.print_exc()
-        return id_list
+                for i in range(1, 3):
+                    try:
+                        data = {
+                            "address": it,
+                            "description": it,
+                            "criticality": "10"
+                        }
+                        data_json = json.dumps(data)
+                        targets_api = '/api/v1/targets'
+                        r = requests.post(self.url + targets_api, data=data_json, headers=self.headers,
+                                          verify=False).text  # 发送扫描任务
+                        id_list.append(json.loads(r)['target_id'])
+                        # targets_adv_conf_api = '/api/v1/targets/%s/configuration' % json.loads(r)['target_id']  #
+                        # 高级配置api data_proxy_api_conf_set = {"proxy": {"enabled": True, "address": "43.228.71.245",
+                        # "protocol": "http", "port": 1111 } } r_conf_proxy = requests.patch(url + targets_adv_conf_api,
+                        # data=json.dumps(data_proxy_api_conf_set), verify=False, headers=headers).text
+
+                        api_run = '/api/v1/scans'
+                        data_run = {
+                            "target_id": json.loads(r)['target_id'],
+                            "profile_id": "11111111-1111-1111-1111-111111111112",
+                            "schedule":
+                                {"disable": False,
+                                 "start_date": None,
+                                 "time_sensitive": False
+                                 }
+                        }
+                        # print(json.dumps(data_run))
+                        r_run = requests.post(self.url + api_run, data=json.dumps(data_run),
+                                              verify=False,
+                                              headers=self.headers).text
+                        # print(r_run)
+                        break
+                    except Exception:
+                        print_err(it)
+                        traceback.print_exc()
+            return id_list
+        except Exception:
+            traceback.print_exc()
+            sendmail(self.mail, 'awvs 扫描出现问题')
 
     def scan_targets(self, urllist: list):  # 检测主线程
 
-        self.awvs_scan(urllist=urllist) # awvs扫描
-        self.nuclei_scan(urllist=urllist) # nuclei扫描
+        self.awvs_scan(urllist=urllist)  # awvs扫描
+        self.nuclei_scan(urllist=urllist)  # nuclei扫描
 
     # print(scan_targets(['baidu.com']))
